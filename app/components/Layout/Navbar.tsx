dbe91f57d23973ec9b9f23abe4283f029d73d806
Erick Pérez Mendoza <erickpm.8d@gmail.com>
2024-06-10 15:15:27 -0600

    modificacación de la navbar y actualización de la página de login

--- app/components/Layout/Navbar.tsx
+++ app/components/Layout/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <div>
       <nav className="navbar bg-body-tertiary">
   <div className="container-fluid">
-    <span className="navbar-brand mb-0 h1">Navbar</span>
+    <span className="navbar-brand mb-0 h1">Barra de navegación</span>
   </div>
 </nav>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,56 @@
-import React from 'react'
-import Card from './components/Card/Card'
-import Layout from './components/Layout/Layout'
+import React from 'react';
+import Link from 'next/link';
+import Layout from './components/Layout/Layout';
+import styles from './styles/Header.module.css'; // Ajusta la ruta según la ubicación real de tu archivo CSS
 
-export default function page() {
+export default function Page() {
   return (
-    <div>
-      <div><Layout></Layout></div>
+    <Layout navbar={true}> {/* Aquí pasas el prop navbar */}
       <div>
-        <h1 className='text-bg-danger'>aefawfa</h1>
-        <Card titulo = "Titulo 1" textoboton='Hola' texto='texto 0' imagen='https://datacenter360.net/wp-content/uploads/2020/02/Firewall-Cisco-Firepower-FPR-C9300-AC.jpg'/>
-        <Card titulo = "Titulo 2" textoboton='Boton 2' texto='texto 1'/>
-        <Card titulo = "Titulo 3" textoboton='Boton 1' texto='texto 2'/>
-        <Card titulo = "Titulo 4" textoboton='Boton 2' texto='texto 3'/>
+        <div className="col-4 d-flex align-items-center justify-content-start">
+          <a className="navbar-brand" href="https://www.acatlan.unam.mx/">
+            <img 
+              src="http://www.w3.org/2000/svg" 
+              alt="Logo Fes Acatlan" 
+              className={`d-inline-block align-text-start img-fluid ${styles.logoImg}`}
+            />
+          </a>
+        </div>
+        
+        <div className="container-sm" style={{ backgroundColor: 'rgba(0, 43, 122, 0.8)', borderRadius: '10px' }}>
+        <div className="container">
+          <div className="row w-100 no-gutters">
+            <div className="col-4 d-flex align-items-center justify-content-start">
+            mundo
+            </div>
+            <div className="col-4 d-flex align-items-center justify-content-center">
+             hola
+            </div>
+            <div className="col-4 align-items-center justify-content-end">
+              <div className="mb-3">
+                ‎ 
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Usuario</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="inputPassword" className="form-label">Contraseña</label>
+                <input type="password" className="form-control" id="inputPassword" />
+              </div>
+              <div className="mb-3">
+                ‎ 
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>  
       </div>
-    </div>
-  )
+    </Layout>
+  );
 }
+
+
+
+
+        
