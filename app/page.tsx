e0542859a084f500de99218e458e160d003c3d27
Erick Pérez Mendoza <erickpm.8d@gmail.com>
2024-06-10 15:36:12 -0600

    actualización de la pagina login

--- app/page.tsx
+++ app/page.tsx
@@ -5,46 +5,48 @@ import styles from './styles/Header.module.css'; // Ajusta la ruta según la ubi
 
 export default function Page() {
   return (
-    <Layout navbar={true}> {/* Aquí pasas el prop navbar */}
-      <div>
-        <div className="col-4 d-flex align-items-center justify-content-start">
-          <a className="navbar-brand" href="https://www.acatlan.unam.mx/">
-            <img 
-              src="http://www.w3.org/2000/svg" 
-              alt="Logo Fes Acatlan" 
-              className={`d-inline-block align-text-start img-fluid ${styles.logoImg}`}
-            />
-          </a>
-        </div>
-        
-        <div className="container-sm" style={{ backgroundColor: 'rgba(0, 43, 122, 0.8)', borderRadius: '10px' }}>
-        <div className="container">
-          <div className="row w-100 no-gutters">
-            <div className="col-4 d-flex align-items-center justify-content-start">
-            mundo
-            </div>
-            <div className="col-4 d-flex align-items-center justify-content-center">
-             hola
-            </div>
-            <div className="col-4 align-items-center justify-content-end">
-              <div className="mb-3">
-                ‎ 
+    <Layout navbar={true}>
+      <div> 
+        <div className="container-sm" style={{ backgroundColor: 'rgba(0, 43, 122, 0.8)', borderRadius: '25px' }}>
+          <div className="container">
+            <div className="row w-100 no-gutters">
+              <div className="col-md-6 d-flex align-items-center justify-content-center">
+                <div className="container-sm" style={{ backgroundColor: 'rgba(239, 202, 69, 1)', borderRadius: '10px' }}>
+                  HOla
+                </div>
               </div>
-              <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Usuario</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="inputPassword" className="form-label">Contraseña</label>
-                <input type="password" className="form-control" id="inputPassword" />
-              </div>
-              <div className="mb-3">
-                ‎ 
+              <div className="col-md-6 d-flex align-items-center justify-content-center">
+                <form>
+                  <div className="mb-3">
+                    ‎ 
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label text-white">Usuario</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="correo@ejemplo.com" />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="inputPassword" className="form-label text-white">Contraseña</label>
+                    <input type="password" className="form-control" id="inputPassword" />
+                  </div>
+                    <button
+                      type="submit"
+                      className="btn w-100"
+                      style={{
+                        backgroundColor: '#EFCA45',
+                        color: '#4F3F05',
+                        borderColor: '#EFCA45',
+                        borderRadius: '20px'
+                      }}>
+                      Ingresar
+                    </button>
+                  <div className="mb-3">
+                    ‎ 
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
-        </div>  
       </div>
     </Layout>
   );
