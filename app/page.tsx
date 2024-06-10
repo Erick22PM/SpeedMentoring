087a6f209885268d35e62cb5309c9fe1657f6640
Erick Pérez Mendoza <erickpm.8d@gmail.com>
2024-06-10 15:49:21 -0600

    actualizacion dela imagen en la pagina de login

--- app/page.tsx
+++ app/page.tsx
@@ -11,26 +11,23 @@ export default function Page() {
           <div className="container">
             <div className="row w-100 no-gutters">
             <div className="col-md-6 d-flex align-items-center justify-content-center">
-  <div className="container-sm d-flex justify-content-center align-items-center" style={{ backgroundColor: 'rgba(239, 202, 69, 1)', borderRadius: '10px' }}>
-    <a className="navbar-brand" href="https://www.acatlan.unam.mx/">
-      <img 
-        src="https://www.acatlan.unam.mx/identidad-acatlan/img/Logotipos/Escudo/escudo-a.png" 
-        alt="Logo Fes Acatlan" 
-        className="img-fluid" 
-        style={{ maxWidth: '100%', maxHeight: '200px' }} // Hacer la imagen responsiva y definir un tamaño máximo
-      />
-    </a>
-  </div>
-</div>
+              <div className="container-sm d-flex justify-content-center align-items-center my-4" style={{ backgroundColor: 'rgba(239, 202, 69, 1)', borderRadius: '10px' }}>
+                <a className="navbar-brand" href="https://www.acatlan.unam.mx/">
+                  <img 
+                    src="https://www.acatlan.unam.mx/identidad-acatlan/img/Logotipos/Escudo/escudo-a.png" 
+                    alt="Logo Fes Acatlan" 
+                    className="img-fluid my-4" 
+                    style={{ maxWidth: '100%', maxHeight: '200px' }} // Hacer la imagen responsiva y definir un tamaño máximo
+                  />
+                </a>
+              </div>
+              </div>
 
-              <div className="col-md-6 d-flex align-items-center justify-content-center">
+              <div className="col-md-6 d-flex align-items-center justify-content-center  my-4">
                 <form>
                   <div className="mb-3">
-                    ‎ 
-                  </div>
-                  <div className="mb-3">
                     <label htmlFor="exampleFormControlInput1" className="form-label text-white">Usuario</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="correo@ejemplo.com" />
+                    <input type="email" className="form-control" id="exampleFormControlInput1"/>
                   </div>
                   <div className="mb-3">
                     <label htmlFor="inputPassword" className="form-label text-white">Contraseña</label>
@@ -47,9 +44,6 @@ export default function Page() {
                       }}>
                       Ingresar
                     </button>
-                  <div className="mb-3">
-                    ‎ 
-                  </div>
                 </form>
               </div>
             </div>
