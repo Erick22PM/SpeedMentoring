bef79314765d3eb1ea48a4e00de752d29e20830c
Erick Pérez Mendoza <erickpm.8d@gmail.com>
2024-06-25 20:11:59 -0600

    Se agregó un botón para cerrar la sesión en las navbars de mentores, estudiantes y admin

--- app/components/Layout/Navbar_admin.tsx
+++ app/components/Layout/Navbar_admin.tsx
@@ -21,6 +21,13 @@ export default function Navbar() {
               <a className="nav-link" style={{ color: '#3A2E01', fontWeight: 'bold' }} href="/Admin/usuarios">Usuarios</a>
             </li>
           </ul>
+          <ul className="nav justify-content-end">
+            <li className="nav-item">
+              <a className="navbar-brand" href="/login">
+                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#3A2E01"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
+              </a>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
